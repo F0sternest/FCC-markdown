3b36fd32a97a0a3079bb0618833e3baeefe2bad4
F0sternest <kevin.rod2026@gmail.com>
2023-05-09 07:54:50 -0500

    cambios malos

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,22 @@ import "./styles/App.css";
 import ReactMarkdown from "react-markdown";
 import { useState } from "react";
 import remarkGfm from "remark-gfm";
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 function App() {
-    const [rawMarkdown, setRawMarkdown] = useState("# Hello");
+    /*MARKDOWN DE EJEMPLO
+    | S/N | Pet | Image |
+    |--|--|--|
+    | 1 | Cat |![A cat looking at you](https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=68615bab04be2077a471009ffc236509) |
+    | 2 | Dog |![A dog looking at you](https://ichef.bbci.co.uk/news/976/cpsprodpb/17638/production/_124800859_gettyimages-817514614.jpg)|
+    
+    - [ ] Task list 1
+    - [ ] Pending task list 2
+    - [x] Completed task list 3
+    - [x] Completed task list 4 
+    
+    - A direct URL: https://www.copycat.dev/*/
+
+    const [rawMarkdown, setRawMarkdown] = useState(`# Hello`);
 
     const handleMarkdown = (event) => {
         setRawMarkdown(event.target.value);
@@ -23,27 +34,8 @@ function App() {
             />
             <div id="preview">
                 <ReactMarkdown
-                    className="markdown-previwer"
-                    children={rawMarkdown}
                     remarkPlugins={[remarkGfm]}
-                    components={{
-                        code({ node, inline, className, children, ...props }) {
-                            const match = /language-(\w+)/.exec(className || "");
-                            return !inline && match ? (
-                                <SyntaxHighlighter
-                                    children={String(children).replace(/\n$/,"")}
-                                    style={docco}
-                                    language={match[1]}
-                                    PreTag="div"
-                                    {...props}
-                                />
-                            ) : (
-                                <code className={className} {...props}>
-                                    {children}
-                                </code>
-                            );
-                        },
-                    }}
+                    children={rawMarkdown}    
                 />
             </div>
         </div>
